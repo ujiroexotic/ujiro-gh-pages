109bd6718930706df16c4cc42a31f095694032f4
ujiroexotic <ujiroeruteya@gmail.com>
2022-07-29 13:07:01 +0200

    First upload via VS Code

---
+++ ujiro_react_tutorial/src/navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h2>EMMI React</h2>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Create Blog</a>
+
+            </div>
+        </nav>
+      );
+}
+ 
+export default Navbar;
